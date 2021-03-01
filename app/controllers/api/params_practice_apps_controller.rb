d13e55e942edab05f9b9a2825ae27f293a084db5
Paige Morton <paigeashley624@gmail.com>
2021-02-28 22:33:15 -0400

    initial commit

---
+++ app/controllers/api/params_practice_apps_controller.rb
@@ -0,0 +1,6 @@
+class Api::ParamsPracticeAppsController < ApplicationController
+  def practice
+
+    render: "practice.json.jb"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    get "/params_practice", controller:"params_practice_apps", action: "practice" 
+end
